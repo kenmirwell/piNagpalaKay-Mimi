624bfb6bc3ccc46f105111d57f34b8baba02e9a3
KenMirwell <kenmirwell@gmail.com>
2026-02-25 14:02:07 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WeddingApp from './Page'
-import Apple from './Sample'
 
 function App() {
   const [count, setCount] = useState(0)
