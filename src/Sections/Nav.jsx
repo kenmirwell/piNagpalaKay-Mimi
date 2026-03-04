43e21d42c88e35209f435700033a511f1c7a539e
KenMirwell <kenmirwell@gmail.com>
2026-03-04 10:51:15 +0800

    update

--- src/Sections/Nav.jsx
+++ src/Sections/Nav.jsx
@@ -4,8 +4,11 @@ import KMlogo from "../assets/KMlogo.png"
 export default function Nav({ scrolled }) {
   return (
     <nav className={`fixed top-0 left-0 right-0 z-50 flex justify-between items-center px-8 md:px-14 py-6 transition-all duration-500 ${scrolled ? "bg-amber-950/80 backdrop-blur-md" : ""}`}>
-      <div style={{fontFamily:"'Great Vibes', cursive", fontSize:"2rem", color:"#fdf6ee", textShadow:"0 1px 3px rgba(0,0,0,0.3)"}}>
-        <img className="w-[100px]" src={KMlogo} alt="km logo" />
+      <div className="flex items-center gap-[10px]" style={{fontFamily:"'Great Vibes', cursive", fontSize:"2rem", color:"#fdf6ee", textShadow:"0 1px 3px rgba(0,0,0,0.3)"}}>
+        {/* <img className="w-[100px]" src={KMlogo} alt="km logo" /> */}
+        K 
+          <span className="block opacity-70" style={{fontSize:"0.6em"}}>&</span>
+         <span className="ml-[-5px]">M</span>
       </div>
       <ul className="hidden md:flex gap-10 list-none">
         {["story","schedule", "details", "gift"].map(s => (
