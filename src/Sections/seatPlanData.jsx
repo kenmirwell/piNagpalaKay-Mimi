51dc4c1af37afcc23fe4df1d622fe74a7195f518
Keed0303 <kydnagpala.dev@gmail.com>
2026-04-06 21:09:39 +0800

    chore: ruwana to roanna

---
+++ src/Sections/seatPlanData.jsx
@@ -0,0 +1,127 @@
+export const TABLES = [
+  {
+    name: "VIPs 1",
+    vip: true,
+    guests: [
+      "Mr. Herminigildo Gecale", "Mrs. Lolita Gecale",
+      "Mr. Ariel Tenorio", "Mrs. Janneth Tenorio",
+      "Mr. Marlon Natividad", "Mrs. Aurea Natividad",
+      "Mr. Poly Christian Cabral", "Mrs. Rea Cabral",
+      "Ms. Lolita Garcia", "Ms. Cynthia Buen",
+    ],
+  },
+  {
+    name: "VIPs 2",
+    vip: true,
+    guests: [
+      "Mrs. Miriam Nagpala", "Lola Mila Ordoñez",
+      "Mrs. Corazon Villar", "Mr. Linberg Villar",
+      "Ms. Lolita Garcia", "Mr. Joshua Gamboa",
+      "Mrs. Anne Jane Gamboa", "Mrs. Marie Mylitt Lim",
+      "Mr. Edward Cedric Lim", "Mr. Eric Barrion",
+      "Mrs. Daisy Barrion"
+    ],
+  },
+  {
+    name: "Table 1",
+    guests: [
+      "Gina Nagpala", "Kierr Nagpala", "Gian Nagpala",
+      "Kyd Nagpala", "Giro Dejan", "Kim Dia",
+      "Tita Malou Dia", "Tito Berting Dia", "Tito Mar Dejan",
+    ],
+  },
+  {
+    name: "Table 2",
+    guests: [
+      "May Gecale", "Marvin Gecale", "Markjohn Gecale",
+      "Rachelle Manaig", "Bianca Escosura",
+      "Cristine Faye Anne Gecale", "Chamylle Jerica Gecale",
+      "Cyrus Dave Gecale",
+    ],
+  },
+  {
+    name: "Table 3",
+    guests: [
+      "John Tomas", "Shien Dullavin",
+      "Nikka Suasba", "Julianne Dawn Paje",
+      "Ally Tenorio", "Angelo Del Rosario",
+      "Patty Leocario", "Cyndy Buen",
+    ],
+  },
+  {
+    name: "Table 4",
+    guests: [
+      "Mary Dian Diaz", "Darlene Diaz", "Jade Lian Gecale", "Darwin Diaz",
+      "Ruwel Gecale", "Roanna Joy Gecale"
+    ],
+  },
+  {
+    name: "Table 5",
+    guests: [
+      "Hermie Barit", "Jhoanna Barit", "Dave Barit",
+      "Cherry Portales", "Gilbert Portales", "Hanna Portales",
+      "AJ De Guzman", "Martin De Guzman",
+    ],
+  },
+  {
+    name: "Table 6",
+    guests: [
+      "Malen Celeste", "Kenneth Celeste", "Kayler Celeste",
+      "Mike Holgado", "Princess Holgado",
+      "Katrina Reyes", "Angelo Reyes",
+      "Marvin Saturno", "Lukas Saturno", "Anna Mae Saturno",
+    ],
+  },
+  {
+    name: "Table 7",
+    guests: [
+      "Love Galvan", "Jae Galvan", "Bong Flores", "Joy Flores",
+      "Anjenneth Tumamak", "Christian Tumamak",
+      "Raquel Barerra", "Moses Barrera"
+    ],
+  },
+  {
+    name: "Table 8",
+    guests: [
+      "Gia Esther Gamboa", "Daniel Natividad",
+      "Julia Michaila Cabral", "Jansen Flores",
+      "Janiel Annaiah Faith Tenorio", "Alyanna Yzabel Tenorio",
+      "Chloe Barrion", "Ethan Cabral",
+    ],
+  },
+  {
+    name: "Table 9",
+    guests: [
+      "Kinjo Joaquin", "Ronald Gacosta", "Rochelle Mentino",
+      "Rheg Verano", "Tin Guerrero", "Joseph Samonte",,
+      "Lenard Visaya",
+    ],
+  },
+  {
+    name: "Table 10",
+    guests: [
+      "Angelo Sarmiento", "Ten Sandoval",
+      "Marlein Montierro", "Ella Tinio",
+      "Sandy Leocario", "Jen Natividad",
+      "Jana Tenorio", "Hannah Gamboa"
+    ],
+  },
+  {
+    name: "Table 11",
+    guests: [
+      "Aprilyn Etorma", "Xhiane F. Esagre",
+      "Raymond Gimutao", "Joy Gimutao", "John Miguel JM Hondo",
+      "Albert Tan", "Abraham Koko Capacete",
+      "Angelo Agapito", "Rhea Agapito",
+    ],
+  },
+  {
+    name: "Kids Table",
+    kids: true,
+    guests: [
+      "Coleen", "Claire", "Hadassah", "Elianna",
+      "Thea", "Massey", "Kelsey",
+      "Nigel", "Gab", "Mattias", "Lukas", "Kayler", "Gian"
+    ],
+  },
+];
